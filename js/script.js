3a5529c32cb56b957c93856f9179e3bd3a4d62ac
Riccardo <riccardolipparini0@gmail.com>
2021-11-12 17:30:15 +0100

    push

---
+++ js/script.js
@@ -0,0 +1,44 @@
+var toDo = new Vue(
+    {
+        el: `#container`,
+        data:{
+            newTask: "",
+            tasks:[
+                {
+                    text: `fare la spesa`,
+                    done: true
+                },
+                {
+                    text: `fare il bucato`,
+                    done: false
+                },
+                {
+                    text: `allenamento`,
+                    done: false
+                },
+                {
+                    text: `andare in posta`,
+                    done: false
+                },
+                {
+                    text: `sbattere la testa contro il muro`,
+                    done: true
+                },
+                {
+                    text: `contemplare il vuoto`,
+                    done: true
+                }
+            ]
+        },
+        methods: {
+            removeItem: function(index){
+                this.tasks.splice(index,1)
+            },
+            aggiungiTask: function(){
+                this.tasks.push(this.newTask);
+                this.newTask = "";
+            }
+        }
+
+
+})
